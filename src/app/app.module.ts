80843d046fa21d75bc4fcf20638496193a25abaa
Phillipe <phillrog@hotmail.com>
2018-04-26 20:09:36 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BotaoDirective } from './componentes/botao.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BotaoDirective
   ],
   imports: [
     BrowserModule
---
+++ src/app/componentes/botao.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BotaoDirective } from './botao.directive';
+
+describe('BotaoDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BotaoDirective();
+    expect(directive).toBeTruthy();
+  });
+});
