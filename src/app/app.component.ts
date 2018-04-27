fdd024e394d856540e3a2e91a75856e3b9aa93e7
Phillipe <phillrog@hotmail.com>
2018-04-26 21:04:19 -0300

    Diretive confirms test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  abrirGoogle() {
+    window.open('http://www.google.com.br', '_blank')
+  }
 }
--- src/app/componentes/botao.directive.ts
+++ src/app/componentes/botao.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, Input } from '@angular/core';
+import { $ } from 'jquery';
+
+
 
 @Directive({
-  selector: '[appBotao]'
+  // tslint:disable-next-line:directive-selector
+  selector: '[btnConfirm]'
 })
 export class BotaoDirective {
+  @Input() messge = 'Deseja realmente efetuar esta ação? ';
+  @Input() btnConfirm = () => { };
 
-  constructor() { }
+  constructor(el: ElementRef, renderer: Renderer) {
+    console.log(el);
+  }
+
+  @HostListener('click', ['$event'])
+  confirmFirst() {
+    const confirmed = window.confirm(this.messge);
+
+    if (confirmed) {
+      this.btnConfirm();
+    }
+  }
+
+
 
 }
